447e418ff89ec38a6680100eed6626e26e70339c
Gustavo Sutana <gustavosutanalima@gmail.com>
2024-10-01 12:58:21 -0300

    Solução do desafio Persist

--- src/strings/challenges/Persist.java
+++ src/strings/challenges/Persist.java
@@ -2,28 +2,40 @@ package strings.challenges;
 
 import java.util.*;
 
-class Persist {
-    public static void main(String[] args) {
-        System.out.println(persistence(123));
-    }
-
+public class Persist {
+   
 	public static int persistence(long n) {
+
+        if (String.valueOf(n).length() == 1) {
+            return 0;
+        }
+        else {
+            int auxContagem = 1;
+            while (String.valueOf(digitsMultiplier(n)).length() > 1) {
+    
+                n = digitsMultiplier(n);
+                auxContagem++;
+            }
+    
+            return auxContagem;
+        }
         
-        return digitsMultiplier(n);
     
 	}
 
-    private static int digitsMultiplier(long n) {
+    static int digitsMultiplier(long n) {
         String nString = Long.toString(n);
 	
         var stringStream = Arrays.stream(nString.split(""));
 
-        int result = 1;
+        long result = 1;
 
-        for (int i : stringStream.mapToInt(Integer::parseInt).toArray()) {
+        for (long i : stringStream.mapToInt(Integer::parseInt).toArray()) {
             result*=i;
         }
-
-        return result;
+    
+        return (int) result;
+        
     }
 }
+ 
